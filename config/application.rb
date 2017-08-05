9b80f1fbd051a0b22d6d0275e13b7e696f20884e
Deepak Kumar <deepakkumarnd@gmail.com>
2017-08-05 21:41:40 +0530

    Minitest testing framework is added

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module Arc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :minitest, spec: false, fixture: false
+    end
+
+    require 'text_processor'
+
   end
 end
