b053e0164f687880cc37c4044148d62ae614275e
Deepak Kumar <deepakkumarnd@gmail.com>
2017-12-01 12:58:58 +0530

    Fix the input output issue in executing the command chain

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery.min
 //= require turbolinks
 //= require cable
 //= require main
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,78 @@
 "use strict";
 
 // function to test es6
-let printName = (name) => {
-    console.log(name.toUpperCase());
+let log = (message) => {
+    console.log(message);
 };
 
-printName("deepak");
+class ArcInputForm {
 
-class Text {
-    constructor(content) {
-        this.content = content;
+    constructor() {
+        this.commands = [];
+        this.textarea = document.getElementById('arc-text-input');
+        this.screen   = document.getElementById('arc-text-output');
+
+        let obj = this;
+
+        $('.arc-action').on('click', (e) => {
+            obj.addCommand(e.target.dataset.command);
+        });
+
+        $('#arc-submit').on('click', (e) => {
+            let data = {
+                commands: obj.commandString,
+                text: obj.inputText
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: '/api/v1/text_operations/run_commands',
+                data: data,
+                success: function(data){ obj.submitCallback(data) },
+                error: function (err) {
+                    console.log(err);
+                    alert("Unable to connect");
+                },
+                dataType: 'json'
+            });
+        });
     }
 
-    test() {
-        console.log(this.content);
+    get commandString() {
+        return this.commands.join('|');
+    }
+
+    addCommand(cmd) {
+        if (!this.commands.includes(cmd)) {
+            this.commands.push(cmd);
+        }
+    }
+
+    get inputText() {
+        return this.textarea.value;
+    }
+
+    set inputText(text) {
+        this.textarea.value = text;
+    }
+
+    set outputScreen(text) {
+        this.screen.innerText = text;
+    }
+
+    submitCallback(data) {
+        if (data.status == 'ok') {
+            this.outputScreen = data.data;
+        } else if (data.status == 'error') {
+            alert(data.message);
+        } else {
+            alert(data.message);
+        }
     }
 }
+
+
+$(document).ready( () => {
+        new ArcInputForm();
+    }
+);
